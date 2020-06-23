3a87eff39de4ee96f11579e55754ec2c4404c2ef
Sergey <33245236+hatabulin@users.noreply.github.com>
2020-06-23 08:53:00 +0300

    Update Utils.c

--- Src/Utils.c
+++ Src/Utils.c
@@ -1,23 +1,18 @@
 /*
  * Utils.cpp
  *
- *  Created on: 8 îêò. 2019 ã.
+ *  Created on: 8 Ã®ÃªÃ². 2019 Ã£.
  *      Author: serg
  */
 
 #include "Utils.h"
 
-void strcopy(char* main, char* second, int pos, int k)
-{
-    if (pos > strlen(main))
-    {
+void strcopy(char* main, char* second, int pos, int k) {
+    if (pos > strlen(main)) {
         second[0] = 0;
         return;
     }
     int i = 0;
-    for (; i < k; ++i)
-    {
-        second[i] = main[pos++];
-    }
+    for (; i < k; ++i) second[i] = main[pos++];
     second[i] = 0;
 }
